f3e5f49861abf687f9715f6c6eccebabd8f038ab
Sheansuke <suerojean@gmail.com>
2020-08-20 18:34:32 -0400

    Updates

---
+++ precache-manifest.735babf7b13758eb7394c92635f5ee3d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fecd442a196e1c1316baf308d845d47",
+    "url": "/fylo-data-storage-component-frontEndMentor/index.html"
+  },
+  {
+    "revision": "c33363f387aa8436698c",
+    "url": "/fylo-data-storage-component-frontEndMentor/static/css/main.0a3f35b1.chunk.css"
+  },
+  {
+    "revision": "02184b279596b81ea3c8",
+    "url": "/fylo-data-storage-component-frontEndMentor/static/js/2.5250cb75.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fylo-data-storage-component-frontEndMentor/static/js/2.5250cb75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c33363f387aa8436698c",
+    "url": "/fylo-data-storage-component-frontEndMentor/static/js/main.f751b632.chunk.js"
+  },
+  {
+    "revision": "28f3d5ea95a0f05e25bf",
+    "url": "/fylo-data-storage-component-frontEndMentor/static/js/runtime-main.c7147d95.js"
+  },
+  {
+    "revision": "dba266ecc00b02679c2491363aca8b26",
+    "url": "/fylo-data-storage-component-frontEndMentor/static/media/bg-desktop.dba266ec.png"
+  },
+  {
+    "revision": "03a2f47890386ea347facb9892a7a1d5",
+    "url": "/fylo-data-storage-component-frontEndMentor/static/media/bg-mobile.03a2f478.png"
+  },
+  {
+    "revision": "db85d9372de3902ba2798d7e10a8fcb5",
+    "url": "/fylo-data-storage-component-frontEndMentor/static/media/icon-document.db85d937.svg"
+  },
+  {
+    "revision": "b76d31f55bbb75eb121a1fe963e50a3c",
+    "url": "/fylo-data-storage-component-frontEndMentor/static/media/icon-folder.b76d31f5.svg"
+  },
+  {
+    "revision": "b7e6adf04dcc6059d9c5cb29fdb7b1d3",
+    "url": "/fylo-data-storage-component-frontEndMentor/static/media/icon-upload.b7e6adf0.svg"
+  },
+  {
+    "revision": "35d2c7c63e4db2296166ac69f20941c9",
+    "url": "/fylo-data-storage-component-frontEndMentor/static/media/logo.35d2c7c6.svg"
+  }
+]);
