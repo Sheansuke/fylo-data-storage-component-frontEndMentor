971ef2c197d6111e2be0c6844402b80b269c1480
Sheansuke <suerojean@gmail.com>
2020-08-20 18:36:24 -0400

    :D

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1bae9ef8f3f0c225dfd20ca03b1efc2c.js"
+  "/fylo-data-storage-component-frontEndMentor/precache-manifest.735babf7b13758eb7394c92635f5ee3d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/fylo-data-storage-component-frontEndMentor/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
